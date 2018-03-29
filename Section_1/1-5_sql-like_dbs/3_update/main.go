b3948381b49bdeaf94e177e9dbcf8a503f61af8a
MacCracken <robert.maccracken@gmail.com>
2018-03-29 13:05:21 -0700

    worked through first section

---
+++ Section_1/1-5_sql-like_dbs/3_update/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	// pq is the libary that allows us to connect
+	// to postgres with databases/sql.
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	// Get my postgres connection URL. I have it stored in
+	// an environmental variable.
+	// connStr := "user=pqgotest dbname=pqgotest sslmode=verify-full" #note sslmode=disabled if note enabled
+	// or
+	// connStr := "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full"
+	pgURL := os.Getenv("PGURL")
+	if pgURL == "" {
+		log.Fatal("PGURL empty")
+	}
+
+	// Open a database value. Specify the postgres driver
+	// for databases/sql.
+	db, err := sql.Open("postgres", pgURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Update some values.
+	res, err := db.Exec("UPDATE iris SET species = 'setosa' WHERE species = 'Iris-setosa'")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// See how many rows where updated.
+	rowCount, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Output the number of rows to standard out.
+	log.Printf("affected = %d\n", rowCount)
+}
